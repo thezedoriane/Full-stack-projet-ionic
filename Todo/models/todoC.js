725f3beeb20447a3a2546e195c4fda9930c48673
thezedoriane <72502637+thezedoriane@users.noreply.github.com>
2021-11-28 17:35:53 +0100

    Create todoC.js

---
+++ Todo/models/todoC.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+    name : String,
+    description : String,
+    shopId : {type:mongoose.Schema.Types.ObjectId,ref:"Shop"},
+    done : {
+      type : Boolean,
+      default : false
+    },
+    createdAt : {
+      type : Date,
+      default : Date.now
+    },
+  });
+
+  module.exports = mongoose.model('Category', CategorySchema);
